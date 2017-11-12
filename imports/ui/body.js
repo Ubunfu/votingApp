4ca400fc047261edeb673cbcbfb617f705d0a560
Ryan Allen <rallen3882@gmail.com>
2017-11-11 20:33:52 -0500

    Playing with helpers and events.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,1 +1,39 @@
+// This imports the "Template" functionality from the meteor packages.
+import {Template} from 'meteor/templating';
+
+/*
+  Import the contents of the body.html file.
+  When this file is imported, body.html will be imported too.
+*/
 import './body.html';
+
+/*
+  These helpers are grabbable from within templates.
+  We can pull data and display it with helpers.
+*/
+Template.body.helpers({
+  firstName: 'Ryan',
+  lastName: 'Allen'
+});
+
+/*
+  Events are formatted like:
+  '<trigger-action> <selector>': <result-action>
+  Events are used to do things, like submit forms, handling click
+  events and updating content.
+*/
+Template.body.events({
+/*
+  es2015 JS syntax allows us to replace "function(event)"
+  with "(event) =>".  It means the same thing.
+
+  'click .btn-logger': (event) => {
+    console.log("You clicked the button...");
+  }
+*/
+
+// New meteor syntax allows us to modify the above like this
+  'click .btn-logger'(event) {
+    console.log("You clicked the button...");
+  }
+});
